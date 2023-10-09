7ae64d88b9a7d9d027a5d0f9b925b4a39e9e7634
fuzhiqiang <fuzhiqiang@baidu.com>
2023-10-09 22:03:25 +0800

    chore: eslint fix

--- dist/extension.js
+++ dist/extension.js
@@ -1,1 +1,80 @@
-(()=>{"use strict";var e={496:e=>{e.exports=require("vscode")}},o={};function t(n){var r=o[n];if(void 0!==r)return r.exports;var i=o[n]={exports:{}};return e[n](i,i.exports,t),i.exports}var n={};(()=>{var e=n;Object.defineProperty(e,"__esModule",{value:!0}),e.activate=void 0;const o=t(496);e.activate=function(e){console.log('Congratulations, your extension "highlight-todo" is now active!');let t=o.workspace.onDidOpenTextDocument((e=>{console.log(e,"document"),function(e){const t=e.getText(),n=/\/\/.*?(TODO|FIXME|XXX):/g,r=[];let i;for(;null!==(i=n.exec(t));){const t=e.positionAt(i.index),n=e.positionAt(i.index+i[0].length),s={range:new o.Range(t,n),hoverMessage:"Todo comment: "+i[0]};r.push(s)}const s=o.window.createTextEditorDecorationType({backgroundColor:"rgba(255, 255, 0, 0.2)"});o.window.activeTextEditor?.setDecorations(s,r)}(e)}));e.subscriptions.push(t)}})(),module.exports=n})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = void 0;
+const vscode = __webpack_require__(1);
+function activate(context) {
+    console.log('Congratulations, your extension "highlight-todo" is now active!');
+    let disposable = vscode.workspace.onDidOpenTextDocument((document) => {
+        console.log(document, 'document');
+        highlightTodos(document);
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+function highlightTodos(document) {
+    const text = document.getText();
+    const todoRegex = /\/\/.*?(TODO|FIXME|XXX):/g;
+    const decorations = [];
+    let match;
+    while ((match = todoRegex.exec(text)) !== null) {
+        const startPos = document.positionAt(match.index);
+        const endPos = document.positionAt(match.index + match[0].length);
+        const decoration = {
+            range: new vscode.Range(startPos, endPos),
+            hoverMessage: 'Todo comment: ' + match[0],
+        };
+        decorations.push(decoration);
+    }
+    const decorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: 'rgba(255, 255, 0, 0.2)',
+    });
+    vscode.window.activeTextEditor?.setDecorations(decorationType, decorations);
+}
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
